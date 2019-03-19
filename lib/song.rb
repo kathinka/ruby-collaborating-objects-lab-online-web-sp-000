9da6341b8dc258d363eacbd4701d1d49835a7802
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-03-19 20:58:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,12 @@ class Song
 @@all =[]
 
   def self.all
-   @@all << self
+   @@all 
   end
 
+def save
+  @@all<< self
+end
   def artist=(artist)
     @artist = artist
     artist.add_song(self) unless artist.songs.include?(self)
