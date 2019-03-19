623275d26dbdce422ed2409321f937070d848af8
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-03-19 20:55:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  #attr_accessor :name, :artist
+  attr_accessor :genre #:name, :artist
   attr_reader :name, :artist
 
   def initialize(name)
@@ -14,6 +14,9 @@ class Song
 
   def self.new_by_filename(filename)
     artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
+    song = self.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = genre_name
   end
 
 end
