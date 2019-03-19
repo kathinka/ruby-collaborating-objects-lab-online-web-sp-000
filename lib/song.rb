5f356d4f81a092924dad1630757dcb5448653a5f
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-03-19 20:53:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,18 @@ class Song
   #attr_accessor :name, :artist
   attr_reader :name, :artist
 
-def initialize(name)
-@name = name
-@artist = nil
-end
+  def initialize(name)
+  @name = name
+  @artist = nil
+  end
 
-def artist=(artist)
-  @artist = artist
-  artist.add_song(self) unless artist.songs.include?(self)
-end
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
 
-def self.new_by_filename(filename)
-  artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
+  def self.new_by_filename(filename)
+    artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
+  end
+
 end
