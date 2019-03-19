8db19fadd78096d2417d166acd41bacf4dfa8450
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-03-19 20:40:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ def initialize(name)
 end
 
 def self.new_by_filename(filename)
-  song = self.new_by_filename
+  song = self.new_by_filename.save
   song.title = filename.split(" - ")[1]
   song.artist = filename.split(" - ")[0]
   song
